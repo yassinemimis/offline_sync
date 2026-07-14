319442ec139c4cca322995d43df715348bc0c078
yassinemimis <yassinekherbouche88@gmail.com>
2026-07-14 17:37:31 +0100

    fix: update site URL and base path for GitHub Pages deployment

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -15,10 +15,9 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+url: 'https://yassinemimis.github.io', 
+  baseUrl: '/offline_sync/',             
+  // highlight-end
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
